dd33dd2121383615526872c3358e7b0be1db5ba9
Andrew Burnett <andrew@andburn.info>
2017-02-24 17:37:52 +0000

    Add basic image viewer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Windows;
 
 namespace ImageWatcher
 {
@@ -23,6 +13,22 @@ namespace ImageWatcher
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+
+			dialog.InitialDirectory = @"C:\";
+			dialog.Filter = "Image files(*.bmp;*.jpg;*.png)|*.bmp;*.jpg;*.png|All files (*.*)|*.*";
+			dialog.FilterIndex = 2;
+			//dialog.RestoreDirectory = true;
+			dialog.Multiselect = false;
+
+			if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				imagebox.Source = new BitmapImage(new Uri(dialog.FileName));
+			}
 		}
 	}
 }
